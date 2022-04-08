f301fc3048cc3dabdfe56ffa0f3da6ebea68bcc3
Kimsohyun93 <shk980903@naver.com>
2022-04-08 16:17:46 +0900

    create value struct

--- src/main/java/io/confluent/connect/jdbc/sink/BufferedRecords.java
+++ src/main/java/io/confluent/connect/jdbc/sink/BufferedRecords.java
@@ -198,6 +198,8 @@ public class BufferedRecords {
               .put("Latitude", jsonMap.get("Latitude") != null ? jsonMap.get("Latitude") : 0.0)
               .put("Longitude", jsonMap.get("Longitude") != null ? jsonMap.get("Longitude") : 0.0)
               .put("Altitude", jsonMap.get("Altitude") != null ? jsonMap.get("Altitude") : 0.0);
+      System.out.println("########HERE\n" + record.valueSchema());
+      System.out.println("########HERE\n" + valueStruct);
       SinkRecord valueRecord =
               new SinkRecord(
                       record.topic(),
